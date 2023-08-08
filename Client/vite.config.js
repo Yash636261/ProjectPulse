62259b86ea26d33b182dac56a1dd44a9c6714fc1
yash suthar <ysuthar8055@gmail.com>
2023-08-08 14:17:58 +0530

    changed the config for deployment

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import image from 'vite-plugin-image'
+import reactRouterPlugin from '@vitejs/plugin-react-router'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), image(), reactRouterPlugin()],
 })
