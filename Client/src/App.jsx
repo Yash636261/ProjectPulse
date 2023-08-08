87a803e73e08bbd769cfe20618f9089abbc83bfc
yash suthar <ysuthar8055@gmail.com>
2023-08-08 14:13:06 +0530

    resolving errors in build

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Login from "./pages/Login/login";
+import Login from "./pages/Login/Login";
 import Profile from "./pages/Profile/Profile";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
