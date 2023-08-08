15f334f274252a8d1ca03b081f0b614a70a65377
yash suthar <ysuthar8055@gmail.com>
2023-08-08 14:38:59 +0530

    config change

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import image from 'vite-plugin-image';
 
 export default defineConfig({
-  plugins: [react(), image()],
+  plugins: [react()],
 });
