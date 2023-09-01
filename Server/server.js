820220f2f344346e07c2614834eb39daef604ab4
yash suthar <ysuthar8055@gmail.com>
2023-09-01 15:09:21 +0530

    connection to database done succesfully

--- Server/server.js
+++ Server/server.js
@@ -4,18 +4,31 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 
+const connectDB = require('./db/connect')
 
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handeler')
 
+//middleware
+app.use(express.json())
 
-
-
-
-const port = 5000;
-
+//routes
 app.get('/', (req, res) => {
-  res.send('Hello from the server!');
+  res.send('<h1>Hello from the server!</h1> <a href="/api/v1/products">products route</a>');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 5000;
+
+const start = async()=>{
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port,console.log(`server is listening of port ${port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start();
