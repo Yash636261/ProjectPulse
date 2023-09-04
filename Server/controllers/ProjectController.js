9ee3086c728d22824e87dc83e4250baaca92cdf5
yash suthar <ysuthar8055@gmail.com>
2023-09-04 19:25:12 +0530

    update project route completed

---
+++ Server/controllers/ProjectController.js
@@ -0,0 +1,54 @@
+const {Admin,Project} = require("../models/ProjectAdmin");
+
+exports.getAllProjects = async (req,res)=>{
+    try{
+        const Projects = await Project.find();
+        res.json(Projects);
+    }catch(err){
+        res.status(500).json({error: 'Internal server error'});
+    }
+}
+
+exports.addProject = async (req,res)=>{
+    try{
+        const {projectName,projectDesc} = req.body;
+
+        const newProject = new Project({
+            projectName,
+            projectDesc
+        });
+
+        const savedSubject = await newProject.save();
+
+        res.json(savedSubject);
+
+    }catch(err){
+        res.status(500).json({error: 'Internal server error'})
+    }
+}
+
+exports.updateProject = async (req, res) => {
+    try {
+        const { _id } = req.params;
+        const { projectName, projectDesc } = req.body;
+
+        // Check if the project exists
+        const existingProject = await Project.findById(_id);
+
+
+        if (!existingProject) {
+            return res.status(404).json({ error: 'Project not found' });
+        }
+
+        // Update project details
+        existingProject.projectName = projectName;
+        existingProject.projectDesc = projectDesc;
+
+        // Save the updated project
+        const updatedProject = await existingProject.save();
+
+        res.json(updatedProject);
+    } catch (err) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
--- Server/routes/ProjectRoutes.js
+++ Server/routes/ProjectRoutes.js
@@ -1,7 +1,8 @@
-const {getAllProjects,addProject} = require('../controllers/AdminController');
+const {getAllProjects,addProject,updateProject} = require('../controllers/ProjectController');
 const router = require('express').Router()
 
 router.get('/allprojects',getAllProjects);
 router.post('/addproject',addProject);
+router.put('/updateproject/:_id',updateProject);
 
 module.exports = router;
