d1b481401b40ddbbc61205fc06c3852975470e89
yash suthar <ysuthar8055@gmail.com>
2023-09-18 22:31:25 +0530

    tasks page completed

--- Client/src/pages/Profile/components/TasksSection.jsx
+++ Client/src/pages/Profile/components/TasksSection.jsx
@@ -1,40 +1,52 @@
-import axios from 'axios';
-import React from 'react'
-import { useState ,useEffect } from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function TasksSection() {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    // Fetch projects from the backend when the component mounts
+    axios
+      .get("http://localhost:5000/api/project/allprojects") // Use axios.get to make a GET request
+      .then((response) => {
+        setProjects(response.data); // Update state with the data from the response
+      })
+      .catch((error) => {
+        console.error("Error fetching projects:", error);
+      });
+  }, []);
 
-  const [projectTask, setProjectTask] = useState();
-
-  useEffect(()=>{
-    axios.get("http://localhost:5000/api/project/allprojects")
-    .then((respose)=>{
-      setProjectTask(respose.data);
-    })
-    .catch(error){
-      console.error("erro fetching the data:", error);
-    }
-  },[]);
-
+  let noTaskFound = true;
 
   return (
     <div>
-          <div className="flex justify-center items-center">
-            <img
-              className="w-8 h-8 mr-2"
-              src="https://img.icons8.com/?size=512&id=rnwtSv4H3pXk&format=png"
-              alt=""
-            />
-            <h1 className="font-bold text-3xl my-5">Tasks</h1>
-          </div>
+      <div className="flex justify-center items-center">
+        <img
+          className="w-8 h-8 mr-2"
+          src="https://img.icons8.com/?size=512&id=rnwtSv4H3pXk&format=png"
+          alt=""
+        />
+        <h1 className="font-bold text-3xl my-5">Tasks</h1>
+      </div>
 
-          <div className="bg-slate-700 hover:bg-slate-600 border-0 rounded-lg py-5 px-3 my-1">
-            <h1 className="font-bold text-lg">Project 1</h1>
-            <p>this project is about something crazy so im very much exited.</p>
+      {projects.map((project) => {
+          if (project.projectTasks) {
+            noTaskFound = false; 
+            return(
+          <div 
+          key={project._id}
+          className="bg-slate-700 hover:bg-slate-600 border-0 rounded-lg py-5 px-3 my-1">
+          <h1 className="font-bold text-lg">{project.projectName}</h1>
+            <p>{project.projectTasks}</p>
           </div>
-        </div>
-  )
+          );}
+          return null;
+      })}
+
+      {noTaskFound && 
+        <p className="text-center">Currently, there are no tasks.</p>
+      }
+    </div>
+  );
 }
 
-export default TasksSection
-
+export default TasksSection;
