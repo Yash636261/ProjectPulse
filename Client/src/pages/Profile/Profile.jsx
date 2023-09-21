53ad01cfe1ab1f69752ba486e4bb57e984311eb3
yash suthar <ysuthar8055@gmail.com>
2023-09-21 14:47:34 +0530

    update

--- Client/src/pages/Profile/Profile.jsx
+++ Client/src/pages/Profile/Profile.jsx
@@ -1,8 +1,11 @@
+import {useAuth0} from '@auth0/auth0-react'
 import { useState } from "react";
 import ProjectSection from "./components/ProjectSection";
 import TasksSection from "./components/TasksSection";
 
 function Profile() {
+  const {user, isAuthenticated} = useAuth0();
+
   const [sectionA, setSectionA] = useState(true);
   const [sectionB, setSectionB] = useState(false);
 
@@ -19,13 +22,25 @@ function Profile() {
     <div className="flex max-lg:flex-col justify-between bg-slate-900 py-20 text-white min-h-screen px-5">
       <div className=" md:w-1/5 mx-auto">
         <div className="flex flex-col items-center mx-auto w-full mb-8">
-          <img
+          
+          {isAuthenticated ?(
+            <div><img
             className="w-20 h-20 md:w-42 md:h-42"
+            src={user.picture}
+            alt=""
+          />
+          <p className="font-bold text-2xl">{user.name}</p>
+          <p className="font-semibold text-xl">{user.email}</p>
+
+            </div>
+
+          ):(<img
+            className="w-20 h-20 md:w-42 md:h-42 border-0 rounded-full"
             src="https://img.icons8.com/?size=512&id=108652&format=png"
             alt=""
           />
-          <p className="font-bold text-2xl">Username</p>
-          <p className="font-semibold text-xl">Username@gmail.com</p>
+
+          )}
           <div className="font-semibold w-44 mx-auto text-black mt-10">
             <button
               onClick={changeSectionA}
